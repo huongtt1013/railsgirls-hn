1ff061295a103741a5a41f94612e931447d3dd23
huongtt1013 <huongtt1013@gmail.com>
2016-04-03 08:45:06 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   get 'pages/info'
-
+resource :user, only: [:edit, :update]
 root :to => redirect('/ideas')
   resources :ideas
   # The priority is based upon order of creation: first created -> highest priority.
